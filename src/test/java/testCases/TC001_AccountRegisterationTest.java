f58245af95e66ef668f59935198b12182e30e944
Meghana Bhushan SN <bhushanmeghana027@gmail.com>
2026-04-03 07:19:21 +0530

    logs generation, config.properties, crossparalleltesting

--- src/test/java/testCases/TC001_AccountRegisterationTest.java
+++ src/test/java/testCases/TC001_AccountRegisterationTest.java
@@ -11,10 +11,16 @@ public class TC001_AccountRegisterationTest extends BaseClass {
 	
 	@Test
 	public void verify_Account_Registration_Page() {
+		logger.info("***Starting TC001_AccountRegisteration***");
+		try {
 		HomePAge hp = new HomePAge(driver);
 		hp.clickAccount();
+		logger.info("Clicked on my account ");
 		hp.clickRegister();
+		logger.info("Clicked on my register link ");
 		AccountRegistrationPage arp = new AccountRegistrationPage(driver);
+		
+		logger.info("Provideng customer details");
 		arp.setFirstName(randomStringGene().toUpperCase());
 		arp.setLasttName(randomStringGene().toUpperCase());
 		arp.setEmail(randomStringGene()+"@gmail.com");
@@ -24,8 +30,16 @@ public class TC001_AccountRegisterationTest extends BaseClass {
 		arp.setConfirmPwd(pass);
 		arp.setPrivacyBox();
 		arp.clickContinue();
+		logger.info("validating expected message");
 		String msg = arp.getConfirmationMsg();
 		Assert.assertEquals(msg, "Your Account Has Been Created!");
+		}
+		catch(Exception e) {
+			logger.error("Test failed");
+			logger.debug("debug logs");
+			Assert.fail();
+		}
+		logger.info("tc0001 finished");
 		
 	}
 
--- src/test/java/testbase/BaseClass.java
+++ src/test/java/testbase/BaseClass.java
@@ -1,22 +1,58 @@
 package testbase;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	protected WebDriver driver;
+	public Logger logger;
+	public Properties p;
 	
 	@BeforeClass
-	public void setup() {
+	@Parameters({"os","browser"})
+	public void setup(String os, String br) throws IOException {
+		
+//		loading the config.properties file
+		FileReader file = new FileReader("./src//test//resourcses//config.properties");
+		p=new Properties();
+		p.load(file);
+		
+		
+		logger  = LogManager.getLogger(this.getClass());
+		switch (br.toLowerCase()) {
+		case "chrome":
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			driver = new EdgeDriver();
+			break;
+		case "firefox":
+			driver = new FirefoxDriver();
+			break;
+		default:
+			System.out.println("Invalid browser name.,.,");
+			return;
+		}
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		
 		driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
+//		after u write conf,properties and add properties and all here 
+//		u can rewrite the above statement as 
+		driver.get(p.getProperty("appURL"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 	}
